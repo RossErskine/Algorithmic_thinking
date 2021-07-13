97da9f46542c1b0ecc28ee96cf71b979190c056b
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2021-07-13 15:37:00 +0100

    first commit
    first commit with food lines challenge and the snowflakes challenge

---
+++ Algorithmic_thinking/Algorithmic_thinking/Algorithmic_thinking.cpp
@@ -0,0 +1,28 @@
+#include "Food_lines.h"
+#include "unique_snowflakes.h"
+
+
+int main()
+{
+	std::cout.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+	try
+	{
+		runUniqueSnowflakes();
+		//runFoodLines();
+		return 0;
+	}
+	catch (const char* msg)
+	{
+		std::cerr << msg << std::endl;
+
+	}
+	catch (const std::ios_base::failure& ex)
+	{
+		std::cerr << "Caught exception: " << ex.what()
+			<< ", error code = " << ex.code() << std::endl;
+	}
+	catch (...)
+	{
+		std::cerr << "exception\n";
+	}
+}
